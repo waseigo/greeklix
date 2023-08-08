29934dcef252f2655a0dacde577377b4140540fb
Isaak Tsalicoglou <isaak@waseigo.com>
2023-08-08 08:57:55 +0300

    mix.exs prepared for hex.pm

--- mix.exs
+++ mix.exs
@@ -7,7 +7,35 @@ defmodule Greeklix.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+
+      # Docs
+      name: "Greeklix",
+      source_url: "https://github.com/waseigo/greeklix",
+      homepage_url: "https://blog.waseigo.com/tags/greeklix/",
+      docs: [
+        # The main page in the docs
+        main: "Greeklix",
+        logo: "./assets/logo.png",
+        extras: ["README.md"]
+      ]
+    ]
+  end
+
+  defp description do
+    """
+    An Elixir library for converting Greek to Greeklish text.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Isaak Tsalicoglou"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/waseigo/greeklix"}
     ]
   end
 
